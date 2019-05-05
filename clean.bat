63cf6a0867faf6b639868cd4c8a2a0e538cd80e9
runi95 <runar.serigstad@gmail.com>
2019-05-05 14:39:56 +0200

    Updated the clean.bat to remove the auto folder

--- clean.bat
+++ clean.bat
@@ -3,4 +3,6 @@ DEL /Q bind.go >nul 2>&1
 DEL /Q bind_darwin_amd64.go >nul 2>&1
 DEL /Q bind_linux_amd64.go >nul 2>&1
 DEL /Q bind_windows_amd64.go >nul 2>&1
+DEL /Q auto >nul 2>&1
+RD auto
 astilectron-bundler cc -v
